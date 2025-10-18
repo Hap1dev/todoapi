38310d3eb939c3ea0f9fe4925198fbfdf2b0942c
Chaitanya Moorjani <chaitanya256@chaitanyas-MacBook-Air.local>
2025-10-18 22:52:58 +0530

    route handlers v1

--- src/index.js
+++ src/index.js
@@ -41,18 +41,57 @@ app.get("/tasks", async (req, res) => {
 });
 
 //get specific task
-app.get("/tasks/:id", (req, res) => {
-
+app.get("/tasks/:id", async (req, res) => {
+	try{
+		const id = parseInt(req.params.id);
+		const result = await db.query("SELECT * FROM tasks WHERE id = $1", [id]);
+		if(result.rows.length !== 0){
+			const task = result.rows[0];
+			res.status(200).json(task);
+		}
+		else{
+			return res.status(404).json({message: `Task with id ${id} not found`});
+		}
+	}catch(error){
+		res.status(500).json({message: error.message});
+	}
 });
 
 //modify existing task
-app.put("/tasks/:id", (req, res) => {
-
+app.put("/tasks/:id", async (req, res) => {
+	try{
+		const id = parseInt(req.params.id);
+		const result = await db.query("SELECT * FROM tasks WHERE id = $1", [id]);
+		var task = {};
+		if(result.rows.length !== 0){
+			task = result.rows[0];
+		}else{
+			return res.status(404).json({message: `Task with id ${id} not found`});
+		}
+		const [title, description, is_done] = [req.body.title || task.title, req.body.description || task.description, req.body.is_done || task.is_done];
+		const updated_at = new Date();
+		const ut = await db.query("UPDATE tasks SET title = $1, description = $2, is_done = $3, updated_at = $4 WHERE id = $5 RETURNING *", [title, description, is_done, updated_at, id]);
+		const updatedTask = ut.rows[0];
+		res.status(200).json(updatedTask);
+	}catch(error){
+		res.status(500).json({message: error.message});
+	}
 });
 
 //delete specific task
-app.delete("/tasks/:id", (req, res) => {
-
+app.delete("/tasks/:id", async (req, res) => {
+	try{
+		const id = parseInt(req.params.id);
+		const result = await db.query("SELECT * FROM tasks WHERE id = $1", [id]);
+		if(result.rows.length !== 0){
+			await db.query("DELETE FROM tasks WHERE id = $1", [id]);
+			res.status(200).json({message: `Task with ${id} deleted successfully`});
+		}else{
+			return res.status(404).json({message: `Task with id ${id} not found`});
+		}
+	}catch(error){
+		res.status(500).json({message: error.message});
+	}
 });
 
 app.listen(PORT, () => {
