f47fa9bf3d633cf83fb6f6e9b854ec8c951beac1
Chaitanya Moorjani <chaitanya256@chaitanyas-MacBook-Air.local>
2025-10-19 22:37:04 +0530

    new db connection method

--- src/db.js
+++ src/db.js
@@ -1,11 +1,22 @@
-import pg from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
 
-const db = new pg.Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: { rejectUnauthorized: false }
 });
 
 export default db;
+
+
+// import pg from "pg";
+
+// const db = new pg.Client({
+//     user: process.env.POSTGRES_USER,
+//     host: process.env.POSTGRES_HOST,
+//     database: process.env.POSTGRES_DATABASE,
+//     password: process.env.POSTGRES_PASSWORD,
+//     port: process.env.POSTGRES_PORT
+// });
+
+// export default db;
