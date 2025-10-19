2263bbf51a15012c007dee8c58bbd661ba93205f
Chaitanya Moorjani <chaitanya256@chaitanyas-MacBook-Air.local>
2025-10-20 00:00:33 +0530

    update db.js to run locally

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Pool } = pkg;
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  // ssl: { rejectUnauthorized: false }
 });
 
 export default db;
